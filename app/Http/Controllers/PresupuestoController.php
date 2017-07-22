a467d25080fdb04cf623f1d1fe52e63afe7030b8
Pablo Lizardo <pablo_lizardo@yahoo.com.ar>
2017-07-22 12:45:56 -0300

    idk

--- app/Http/Controllers/PresupuestoController.php
+++ app/Http/Controllers/PresupuestoController.php
@@ -6,38 +6,17 @@ use Illuminate\Http\Request;
 use App\Presupuesto;
 use Session;
 
-class PresupuestoController extends Controller
-{
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+class PresupuestoController extends Controller {
     // public function __construct() {$this->middleware('auth'); }
-    
-    public function index()
-    {
+    public function index() {
         
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
+    public function create() {
         return view('presupuestos.create');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
+    public function store(Request $request) {
         $presupuesto = new Presupuesto;
         $presupuesto->fill($request->all());
         $presupuesto->save();
@@ -46,51 +25,18 @@ class PresupuestoController extends Controller
         return redirect('/');
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
+    public function show($id) {
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
+    public function edit($id) {
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
+    public function update(Request $request, $id) {
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
+    public function destroy($id) {
         $presupuesto = Presupuesto::findOrFail($id);
         $presupuesto->delete();
-
         return redirect('/admin');
     }
 }
